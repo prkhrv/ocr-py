c69e0f09d3774ecf9272204278978691e3856464
prkhrv <prakharnegative@gmail.com>
2021-04-13 17:18:41 +0530

    pan function added

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def ocr_core(filename):
     This function will handle the core OCR processing of images.
     """
     text = pytesseract.image_to_string(Image.open(filename))  # We'll use Pillow's Image class to open the image and pytesseract to detect the string in the image
-    return text
 
-print(ocr_core('images/pan.jpeg'))
+    if pan in text and name in text:
+        return True
+    else:
+        return False
+
+print(ocr_core('images/my.jpg'))
